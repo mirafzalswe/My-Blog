a35d019abc254f15aba54b7a8396fe921cd693f3
mirafzalswe <mirafzaldevelop@gmail.com>
2024-06-06 19:47:29 +0500

    added get most commented comment

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from django.db.models import Count
 
 register = template.Library()
 @register.simple_tag
@@ -11,3 +12,10 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comment=Count('comments')
+    ).order_by('-total_comment')[:count]
